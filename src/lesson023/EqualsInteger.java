a748c4cbf81c7b86cf80cab173941320abd6b093
Anton Prokhorov <vziks@live.ru>
2019-02-14 23:22:45 +0300

    +

--- src/lesson023/EqualsInteger.java
+++ src/lesson023/EqualsInteger.java
@@ -5,31 +5,29 @@ import java.util.Objects;
 public class EqualsInteger {
 
     public static void main(String[] args) {
+//        Integer equals01 = new Integer(7);
         Integer equals01 = 7;
+
+        System.out.println("s01.getClass().getName(); = " + equals01.getClass().getName());
+
         Integer equals02 = equals01;
 
         System.out.println("equals01.equals(equals02) = " + equals01.equals(equals02));
-        System.out.print("equals01==equals02 = ");
-        System.out.println(equals01 == equals02);
-
+        System.out.printf("equals01==equals02 = %b \n", equals01 == equals02);
 
         equals01++;
-//        equals01 = "Strings";
 
+        System.out.println("equals01 = " + equals01);
+        System.out.println("equals02 = " + equals02);
         System.out.println("equals01.equals(equals02) = " + equals01.equals(equals02));
-        System.out.print("equals01==equals02 = ");
-        System.out.println(equals01 == equals02);
+        System.out.printf("equals01==equals02 = %b \n", equals01 == equals02);
 
         equals01 = 7;
 
+        System.out.println("equals01 = " + equals01);
+        System.out.println("equals02 = " + equals02);
+
         System.out.println("equals01.equals(equals02) = " + equals01.equals(equals02));
-        System.out.print("equals01==equals02 = ");
-        System.out.println(equals01 == equals02);
+        System.out.printf("equals01==equals02 = %b \n", equals01 == equals02);
     }
 }
-//equals01.equals(equals02) = true
-//        equals01==equals02 = true
-//        equals01.equals(equals02) = false
-//        equals01==equals02 = false
-//        equals01.equals(equals02) = true
-//        equals01==equals02 = true
--- src/lesson023/EqualsString.java
+++ src/lesson023/EqualsString.java
@@ -1,45 +1,34 @@
 package lesson023;
 
-import java.util.Objects;
 
 public class EqualsString {
+
+
     public static void main(String[] args) {
 
 
-        {
+        String equals01 = new String("String");
+//        String equals01 = "String";
 
-            String s01 = new String("String");
-            String s02 = "String";
+        String equals02 = equals01;
+        System.out.println("Class объекта equals01 = " + equals01.getClass().getName());
+        System.out.println("Class объекта equals02 = " + equals02.getClass().getName());
 
-
-            System.out.println(s01 == s02);
-
-            System.out.println(new String("String") == "String");
-//            String equals01 = new String("String");
-            String equals01 = "String";
-
-            System.out.println("s01.getClass().getName(); = " + s01.getClass().getName());
-
-            String equals02 = equals01;
-
-            System.out.println("equals01.equals(equals02) = " + equals01.equals(equals02));
-            System.out.print("equals01==equals02 = ");
-            System.out.println(equals01 == equals02);
+        System.out.println("Equals сравнение equals01 и equals02 = " + equals01.equals(equals02));
+        System.out.printf("Равенство equals01 и equals02 = %b \n", equals01 == equals02);
 
 //            equals01 = equals01 + new String("s");
-//            equals01 = equals01.concat("s");
-            equals01 = equals01 + "s";
-
-            System.out.println("equals01.equals(equals02) = " + equals01.equals(equals02));
-            System.out.print("equals01==equals02 = ");
-            System.out.println(Objects.equals(equals01, equals02));
+//        equals02 = equals02 + "s";
+        equals01 = equals01 + "s";
+        System.out.println("Добавляем букву");
+        System.out.println("Equals сравнение equals01 и equals02 = " + equals01.equals(equals02));
+        System.out.printf("Равенство equals01 и equals02 = %b \n", equals01 == equals02);
 
 //            equals01 = new String("String");
-            equals01 = "String";
+        equals01 = "String";
+//        equals01 = "String";
 
-            System.out.println("equals01.equals(equals02) = " + equals01.equals(equals02));
-            System.out.print("equals01==equals02 = ");
-            System.out.println(equals01 == equals02);
-        }
+        System.out.println("Equals сравнение equals01 и equals02 = " + equals01.equals(equals02));
+        System.out.printf("Равенство equals01 и equals02 = %b \n", equals01 == equals02);
     }
 }
