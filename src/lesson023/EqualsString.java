0ae677ad725ed431a54f04ac4baa0e3334321db0
Anton Prokhorov <vziks@live.ru>
2019-02-14 23:52:40 +0300

    Equals strings

--- src/lesson023/EqualsString.java
+++ src/lesson023/EqualsString.java
@@ -6,13 +6,24 @@ public class EqualsString {
 
     public static void main(String[] args) {
 
+//        MyObj o1 = new MyObj();
+//        MyObj o2 = o1;
+//
+//        System.out.println("o1.i = " + o1.i);
+//        System.out.println("o2.i = " + o2.i);
+//
+//        o2.i = 7;
+//
+////        o2.method();
+////        o2.method();
+//
+//        System.out.println("o1.i = " + o1.i);
+//        System.out.println("o2.i = " + o2.i);
 
-        String equals01 = new String("String");
-//        String equals01 = "String";
+        String equals01 = new String("Blaaaaaa");
+//        String equals01 = "Blaaaaaa";
 
         String equals02 = equals01;
-        System.out.println("Class объекта equals01 = " + equals01.getClass().getName());
-        System.out.println("Class объекта equals02 = " + equals02.getClass().getName());
 
         System.out.println("Equals сравнение equals01 и equals02 = " + equals01.equals(equals02));
         System.out.printf("Равенство equals01 и equals02 = %b \n", equals01 == equals02);
@@ -25,10 +36,18 @@ public class EqualsString {
         System.out.printf("Равенство equals01 и equals02 = %b \n", equals01 == equals02);
 
 //            equals01 = new String("String");
-        equals01 = "String";
+        equals01 = "Blaaaaaa";
 //        equals01 = "String";
 
         System.out.println("Equals сравнение equals01 и equals02 = " + equals01.equals(equals02));
         System.out.printf("Равенство equals01 и equals02 = %b \n", equals01 == equals02);
+    }
+}
+
+
+class MyObj {
+    int i = 5;
+    public void method (){
+        this.i++;
     }
 }
